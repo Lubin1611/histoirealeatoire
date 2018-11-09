d8df7b9075588b565314948c8a2e9290fedd3c8e
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-11-09 10:20:22 +0100

    Mon tableau objet

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var noms = ["Michel Varuk", "Jim Carrey", "Emma Watson", "Renaud", "Jacques", "Bernadette", "Léa", "Wina", "Jacquie",
     "Michel", "H.P Lovecraft", "Bill Gates", "Gotaga", "Phillipe Etchebest", "Julien Lepers", "Hugh Jackman", "Johnny Deep",
-    "The Rock", "Billy Obam", "Cyrille Vin", "Eminem", "Paul Rudd", "Albert Einstein", "Vivaldi"]
+    "The Rock", "Billy Obam", "Cyrille Vin", "Eminem", "Paul Rudd", "Albert Einstein", "Vivaldi"];
+
+
+var objets = ["ordinateur", "téléphone", "ordinateur", "dé à coudre", "clavier", "tasse", "marteau", "pièce", "manette", "clavier", "ordinateur", "vélo",
+    "avion", "miche de pain", "fourchette", "tasse", "pièces jaunes", "lunettes de soleil", "décapsuleur", "ballon de foot", "téléphone portable", "montre"];
+

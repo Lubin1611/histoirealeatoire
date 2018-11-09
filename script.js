be024b372a28e3555b773f6ceb0926fc4528604b
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2018-11-09 10:17:13 +0100

    Histoire v1

---
+++ script.js
@@ -0,0 +1,3 @@
+var noms = ["Michel Varuk", "Jim Carrey", "Emma Watson", "Renaud", "Jacques", "Bernadette", "Léa", "Wina", "Jacquie",
+    "Michel", "H.P Lovecraft", "Bill Gates", "Gotaga", "Phillipe Etchebest", "Julien Lepers", "Hugh Jackman", "Johnny Deep",
+    "The Rock", "Billy Obam", "Cyrille Vin", "Eminem", "Paul Rudd", "Albert Einstein", "Vivaldi"]

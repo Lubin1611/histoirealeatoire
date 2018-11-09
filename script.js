c42a8eba0969f78625f71a59aca0e5c47811b23a
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-11-09 10:34:14 +0100

    Mon tableau lieux

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ var objets = ["ordinateur", "téléphone", "ordinateur", "dé à coudre", "clavi
 
 var degres = ["10°C", "30°C", "-50°C", "5°C" , "69°C", "18°C", "30°C", "40°C", "69°C", "70°C", "5°C", "28°C", "28°C", "24°C"
 , "37°C", "25°C", "12°C", "25°C", "100°C", "22°C", "37,2°C", "100°F", "30°C", "-5°C"];
+
+var lieux = ["Floyon", "Wignehies", "Barcelona", "Paris", "Tokyo", "Reykjavik", "Mons", "Le Touquet", "Trélon", "Fourmies", "Palais de l'Elysée",
+    "Mairie de Paris", "Mac Do", "Buger King", "Chuttes du Niagara", "Hidepark", "Paris", "Montcuq", "Mac Do", "KFC", "Big Ben", "Mt Fuji",
+    "Bondy", "Rio de Janeiro"];
